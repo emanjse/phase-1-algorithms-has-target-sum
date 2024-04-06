f2278e68ebc4e47dab897aa269b22174760e4ea1
Eman Jamal <emanjse@gmail.com>
2024-04-06 00:52:34 -0700

    done

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  // Iterate over each element in the array
+  for (let i = 0; i < array.length; i++) {
+    // For each element, iterate over the rest of the array
+    for (let j = i + 1; j < array.length; j++) {
+      // If the sum of the current element and any other element equals the target, return true
+      if (array[i] + array[j] === target) {
+        return true;
+      }
+    }
+  }
+  // If no such pair is found, return false
+  return false;
 }
 
-/* 
-  Write the Big O time complexity of your function here
-*/
+/*
+  Write the Big O time complexity of your function here:
+  The time complexity of this function is O(n^2), where n is the length of the array. This is because we have nested loops where we iterate over each element of the array, and for each element, we iterate over the rest of the array.
 
-/* 
-  Add your pseudocode here
 */
 
 /*
-  Add written explanation of your solution here
+  Add your pseudocode here:
+  - Iterate over each element in the array
+  - For each element, iterate over the rest of the array
+  - If the sum of the current element and any other element equals the target, return true
+  - If no such pair is found, return false
+*/
+
+/*
+  Add written explanation of your solution here:
+  - We iterate over each element in the array.
+  - For each element, we iterate over the rest of the array to find another element to form a pair.
+  - If we find a pair whose sum equals the target, we return true.
+  - If no such pair is found after iterating through all elements, we return false.
 */
 
 // You can run `node index.js` to view these console logs
@@ -29,6 +51,26 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([-7, 10, 4, 8], 3));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([1, 2, 3, 4], 5));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([2, 2, 3, 3], 4));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([4], 4));
 }
 
 module.exports = hasTargetSum;
